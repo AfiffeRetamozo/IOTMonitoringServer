7fab45d67aab61fbeb3e8d5fa643bd336fc14b39
Afiffe Retamozo <afiffe.retamozo.a@gmail.com>
2023-09-25 17:50:55 -0500

    change ip

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.238.40.56"]
+ALLOWED_HOSTS = ["localhost", "3.86.157.193"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.93.20.224",  # Dirección IP de la base de datos
+        "HOST": "52.91.204.239",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.93.80.215"
+MQTT_HOST = "44.202.28.61"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
